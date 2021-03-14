f88a187aa7f207e720631e440f0a16e1603661e4
AimDanilaMachin3 <58563393+AimDanilaMachin3@users.noreply.github.com>
2021-03-14 21:32:25 +0400

    2

---
+++ OSI2/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace OSI2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button1.Location = new System.Drawing.Point(32, 74);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 52);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Чтение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button2.Location = new System.Drawing.Point(32, 236);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(120, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Запись";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.richTextBox1.Location = new System.Drawing.Point(32, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(120, 45);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.textBox1.Location = new System.Drawing.Point(32, 170);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 46);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox2.Location = new System.Drawing.Point(197, 25);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(115, 32);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(29, 319);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 371);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OSI2/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OSI2
+{
+    public partial class Form1 : Form
+    {
+        const int PROCESS_VM_WRITE = 0x0020;
+        const int PROCESS_VM_OPERATION = 0x0008;
+        const int PROCESS_WM_READ = 0x0010;
+
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool WriteProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        public static extern bool ReadProcessMemory(int hProcess, Int64 lpBaseAddress, byte[] lpBuffer, int dwSize, ref int lpNumberOfBytesRead);
+
+        //
+        //апи функция для просмотра памяти
+        [DllImport("kernel32.dll")]
+        //для выделения памяти
+        public static extern IntPtr GlobalAlloc(int con, int size);
+        [DllImport("kernel32.dll")]
+        //для освобождения
+        public static extern int GlobalFree(IntPtr start);
+        [DllImport("kernel32.dll")]
+        public static extern void GlobalMemoryStatus(ref MEMORYSTATUS lpBuffer);
+        //
+        public struct MEMORYSTATUS
+        {
+            public UInt32 dwLength;               //Размер структуры, в байтах-хз че это
+            public UInt32 dwMemoryLoad;           //процент занятой памяти
+            public UInt32 dwTotalPhys;            //общее кол-во физической(оперативной) памяти
+            public UInt32 dwAvailPhys;            //свободное кол-во физической(оперативной) памяти
+            public UInt32 dwTotalPageFile;        //предел памяти для системы или текущего процесса
+            public UInt32 dwAvailPageFile;        //Максимальный объем памяти,который текущий процесс может передать в байтах.
+            public UInt32 dwTotalVirtual;         //общее количество виртуальной памяти(файл подкачки)
+            public UInt32 dwAvailVirtual;         //свободное количество виртуальной памяти(файл подкачки)
+            public UInt32 dwAvailExtendedVirtual; //Зарезервировано. Постоянно 0.
+        }
+        //
+        public Form1()
+        {
+            InitializeComponent();
+            textBox2.Text += "0x";
+
+            PerformanceCounter ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use", String.Empty);
+            label1.Text = "Используемая память " + ramCounter.NextValue() + "%";
+
+
+
+           // MEMORYSTATUS memStatus = new MEMORYSTATUS();
+            //label1.Text = "Свободная память файлов подкачки: " + memStatus.dwTotalPageFile.ToString();
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int64 index = Convert.ToInt64(textBox2.Text, 16);
+            Process process = Process.GetProcessesByName("notepad")[0];
+            IntPtr processHandle = OpenProcess(PROCESS_WM_READ, false, process.Id);
+            int bytesRead = 0;
+            byte[] buffer = new byte[24];
+            ReadProcessMemory((int)processHandle, index, buffer, buffer.Length, ref bytesRead);
+            String result = Encoding.Unicode.GetString(buffer);
+            richTextBox1.Text = result;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Int64 index = Convert.ToInt64(textBox2.Text, 16);
+            Process process = Process.GetProcessesByName("notepad")[0];
+            string buf = textBox1.Text;
+            IntPtr processHandle = OpenProcess(0x1F0FFF, false, process.Id);
+            int bytesWritten = 0;
+            byte[] buffer = Encoding.Unicode.GetBytes(buf);
+            WriteProcessMemory((int)processHandle, index, buffer, buffer.Length, ref bytesWritten);
+        }
+    }
+}
